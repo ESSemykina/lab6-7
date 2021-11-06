cd3035425d194a162706622bdda6028982d805ef
ESSemykina <90087300+ESSemykina@users.noreply.github.com>
2021-11-06 11:12:22 +0200

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -7,10 +7,10 @@ int main()
 	int min = mas[0][0];
 	std::cin >> n >> m;
 	ks::vvod(n, m, mas);
-	ks:poiskmin(min, mas, n, m);
-	ks:poiskprost(chislo, n, m, mas, povtor1, prost);
-	ks:umnojmat(mas2, n, m, mas);
-	ks:chetmin(povtor, min, mas, m, n);
-	ks:proverka(povtor, povtor1, mas2, mas, n, m);
-	ks:output(mas, n, m);
+	ks::poiskmin(min, mas, n, m);
+	ks::poiskprost(chislo, n, m, mas, povtor1, prost);
+	ks::umnojmat(mas2, n, m, mas);
+	ks::chetmin(povtor, min, mas, m, n);
+	ks::proverka(povtor, povtor1, mas2, mas, n, m);
+	ks::output(mas, n, m);
 }
