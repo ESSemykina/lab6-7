ebbc317febad2eb994b310f815ac1b502c84539d
ESSemykina <90087300+ESSemykina@users.noreply.github.com>
2021-11-06 11:01:30 +0200

    Update SomeFunctions.hpp

--- SomeFunctions.hpp
+++ SomeFunctions.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #define N 100
 #define M 100
-void vvod(int& n, int& m, int mas[N][M]);
-void poiskmin(int& min, int mas[N][M], int n, int m);
-void poiskprost(bool chislo, int n, int m, int mas[N][M], int& povtor1, int& prost);
-void umnojmat(int mas2[N], int n, int m, int mas[N][M]);
-void chetmin(int& povtor, int min, int mas[N][M], int m, int n);
-void proverka(int povtor, int povtor1, int mas2[N], int mas[N][M], int n, int m);
-void output(int mas[N][M], int n, int m);
+namespace ks
+{
+  void vvod(int& n, int& m, int mas[N][M]);
+  void poiskmin(int& min, int mas[N][M], int n, int m);
+  void poiskprost(bool chislo, int n, int m, int mas[N][M], int& povtor1, int& prost);
+  void umnojmat(int mas2[N], int n, int m, int mas[N][M]);
+  void chetmin(int& povtor, int min, int mas[N][M], int m, int n);
+  void proverka(int povtor, int povtor1, int mas2[N], int mas[N][M], int n, int m);
+  void output(int mas[N][M], int n, int m);
+}
